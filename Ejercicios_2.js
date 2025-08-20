dd35d451eb4c2279085455850b1f23e3c709e581
kotonitro <kotonitro554@gmail.com>
2025-08-20 13:42:38 -0400

    Desarrollo ejercicio 2

---
+++ Ejercicios_2.js
@@ -0,0 +1,9 @@
+//definimos las variables dadas por el enunciado
+const numero1 = 20;
+const numero2 = 40;
+//resolvemos los problemas
+console.log("La suma de",numero1, "con", numero2, "es", numero1 + numero2);
+console.log("La resta de", numero1, "con", numero2, "es", numero1 - numero2);
+console.log("La multiplicación de", numero1, "con", numero2, "es", numero1 * numero2);
+console.log("La división de", numero1, "con", numero2, "es", numero1 / numero2);
+console.log("El módulo de", numero1, "con", numero2, "es", numero1 % numero2);
