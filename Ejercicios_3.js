4af3fa8a680fbec26c1630c006179117cfe80f0c
kotonitro <kotonitro554@gmail.com>
2025-08-20 14:21:37 -0400

    Modificacion de ejercicio 3

--- Ejercicios_3.js
+++ Ejercicios_3.js
@@ -10,26 +10,14 @@ const numero3 = '25';
 5. Muestre por consola si el numero3 es estrictamente diferente que numero1.
 6. Muestre por consola si el numero1 es estrictamente igual al numero2.
 */
-if(numero1 >= numero2){
-  console.log("El numero 1 es mayor o igual que el numero 2")
-}
+console.log(numero1 >= numero2);
 
-if(numero1 <= numero2){
-  console.log("el numero 1 es menor o igual que el numero 2");
-}
+console.log(numero1 <= numero2);
 
-if(numero1 < numero3){
-  console.log("El numero 1 es menor que el numero 3");
-}
+console.log(numero1 < numero3);
 
-if(numero3 < numero2){
-  console.log("El numero 3 es menor que el numero 2");
-}
+console.log(numero3 < numero2);
 
-if(numero3 !== numero1){
-  console.log("El numero 3 es estrictamente diferente al numero 1");
-}
+console.log(numero3 !== numero1);
 
-if(numero1 === numero2){
-  console.log("El numero 1 es estrictamente igual al numero 2");
-}
+console.log(numero1 === numero2);
