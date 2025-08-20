b07067c83e4b4e487ea6baf85dab589e9657b69b
kotonitro <kotonitro554@gmail.com>
2025-08-20 13:26:28 -0400

    Desarrollo ejercicio 1

---
+++ Ejercicios_1.js
@@ -0,0 +1,8 @@
+// Definimos las variables
+const nombre = "Pipo"; // variable nombre tipo STRING 
+let edad = 14; // variable edad tipo NUMBER
+let puedoConducir = true; // variable puedoConducir tipo BOOLEAN
+// Mostramos el contenido de las variables y su tipo
+console.log("La variable", nombre, "es de tipo", typeof nombre);
+console.log("La variable", edad, "es de tipo", typeof edad);
+console.log("La variable", puedoConducir, "es de tipo", typeof puedoConducir);
